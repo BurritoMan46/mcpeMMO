a3303253d06c9593b186b15a2bd7dcf3f8692851
MasterlazorX <beckgilbert4501@gmail.com>
2016-02-29 20:47:56 -0600

    Fixed plugin class not found :)

--- src/mcpeMMO/mcpeMMO.php
+++ src/mcpeMMO/mcpeMMO.php
@@ -33,7 +33,7 @@ use mcpeMMO\party\PartyManager;
 use mcpeMMO\party\ShareHandler;
 use mcpeMMO\skills\SkillManager; /* in the Skill Manager links all the other skills */
 
-class Main extends PluginBase{
+class mcpeMMO extends PluginBase{
     
     public function onEnable(){
         $this->getLogger()->info("mcpeMMO has been enabled.");
